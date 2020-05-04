ebb05a5a38b89896c492a341b58c2289361571cc
Denys_Moiseienko <denis_m_@mail.ru>
2020-05-04 15:38:02 +0200

    add third solution

---
+++ src/main/java/BallUpwards/Ball.java
@@ -0,0 +1,28 @@
+package BallUpwards;
+
+public class Ball {
+  public static int maxBall(int v0) {
+
+    double v = (double) v0 * 1000 / 3600;
+    double time = 0.0;
+    boolean condition = true;
+    double result = 0;
+    int count = 0;
+
+    while (condition){
+      double h = getH(v, time);
+      time += 0.1;
+
+      if(h < result){
+        return count - 1;
+      }
+      count += 1;
+      result = h;
+    }
+    return (int)result;
+  }
+
+  static double getH(double v, double time){
+    return (v * time) - (0.5 * 9.81 * time * time);
+  }
+}
